6b5adf3abaa9fe07d491dc8c33f454296e162277
Junghoon Lee <jeonghunn1@gmail.com>
2019-06-09 14:39:50 +0900

    Loading error page has been fixed.

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ if ($act_parameter != null && !$loaded) {
 function checkLoaded(){
 	global $loaded;
 	$error_code = 404;
-	if(!$loaded) require 'pages/error.php'; 
+    if (!$loaded) require 'pages/core/error.php';
 }
 
 
